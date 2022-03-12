588c19175370930d2653617370b0ee9002ae6e6c
Ishtiaque Matin <nurul_matin@hotmail.com>
2022-03-11 22:30:57 -0500

    Add files via upload

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Welcome to the New Class");
+		System.out.println("Matin");
+		int a=5;
+		int b=6;
+		int c=a+b;
+		System.out.println(c);
+		
+	}
+
+}
---
+++ src/HelloWorldGit.java
@@ -0,0 +1,11 @@
+
+public class HelloWorldGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("Hello World with Git");
+		System.out.print("How is everyone doing");
+	}
+
+}
